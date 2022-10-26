5b58b19eef3aedcd86b07b69d8dbee978392669a
USRVI-LC2 <USRVI-LC2@LC060200.duoc.acad>
2022-10-26 20:42:27 -0300

    ejercicio 5.1.2

--- src/Vista/Form_Ventana2.java
+++ src/Vista/Form_Ventana2.java
@@ -5,12 +5,16 @@
  */
 package Vista;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author USRVI-LC2
  */
 public class Form_Ventana2 extends javax.swing.JFrame {
 
+    
+    int cont = 0;
     /**
      * Creates new form Form_Ventana2
      */
@@ -27,21 +31,75 @@ public class Form_Ventana2 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jlbl_mensaje = new javax.swing.JLabel();
+        jbtn_clic = new javax.swing.JButton();
+        jbtn_salir = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jlbl_mensaje.setText("0");
+
+        jbtn_clic.setText("Clic me");
+        jbtn_clic.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtn_clicActionPerformed(evt);
+            }
+        });
+
+        jbtn_salir.setText("Salir");
+        jbtn_salir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtn_salirActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jbtn_clic, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jlbl_mensaje, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(28, 28, 28)
+                .addComponent(jbtn_salir)
+                .addContainerGap(41, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 352, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jbtn_clic)
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlbl_mensaje)
+                    .addComponent(jbtn_salir))
+                .addContainerGap(27, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jbtn_salirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtn_salirActionPerformed
+        int op = JOptionPane.showConfirmDialog(null, "¿Estas seguro de salir?","Aviso",1);
+        
+        if (op == 0) {
+            System.exit(0);
+        }
+        
+        
+    }//GEN-LAST:event_jbtn_salirActionPerformed
+
+    private void jbtn_clicActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtn_clicActionPerformed
+        
+        
+        
+        cont++;
+        
+        this.jlbl_mensaje.setText("Contar: " + cont);
+        
+    }//GEN-LAST:event_jbtn_clicActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +137,8 @@ public class Form_Ventana2 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jbtn_clic;
+    private javax.swing.JButton jbtn_salir;
+    private javax.swing.JLabel jlbl_mensaje;
     // End of variables declaration//GEN-END:variables
 }
